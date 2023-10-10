6c5dfdb0055941fffe847f6380b4361670468979
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-10-10 18:07:52 -0500

    comentarios

--- src/main/java/co/edu/uniquindio/poo/Triangulo.java
+++ src/main/java/co/edu/uniquindio/poo/Triangulo.java
@@ -3,7 +3,7 @@ package co.edu.uniquindio.poo;
 import javax.swing.JOptionPane;
 
 /* clase triangulo que resive la herencia de (FiguraRegular) y implementa (IfigulaGeometrica)
- * @autor Samuel castaño, Salome Perez, 
+ * @autor Samuel castaño, Salome Perez, Daniel jurado
  *  10/10/2023
  *  licencia publica Nu1.0
  */ 
@@ -13,26 +13,26 @@ public class Triangulo extends FiguraRegular implements IfigulaGeometricaR{
     public Triangulo(float medidaLados) {
         super(medidaLados);
     }
-    // area=base * altura / 2
+    // metodo sobre escrito area=base * altura / 2
     @Override
     public void calcularArea() {
          float area = ((getMedidaLados()*getMedidaLados())*2);
          JOptionPane.showMessageDialog(null, "El area de este  triangulo es de :"+ area);
     }
-    // perimetro= medidalaos sumada 3 veces 
+    // metoso sobre escrito perimetro= medidalados sumado 3 veces 
     @Override
     public void calcularPerimetro() {
         float perimetro= getMedidaLados()+getMedidaLados()+getMedidaLados();
         JOptionPane.showMessageDialog(null, "El perimetro de el triangulo es :s"+perimetro);
     }
-    //altura  = (2A) / b
+    //metodo sobre escrito altura = (2A) / b
     @Override
     public void calcularAlturaTriangulo() {
         int area=0;
         float  triangulo  = (2*area)/getMedidaLados();
         JOptionPane.showMessageDialog(null,"La altura del triangulo es:" +triangulo );
     }
-    //apotema = área / (perímetro / 2)
+    // metodo sobre escrito apotema = área / (perímetro / 2)
     @Override
     public void calcularLongitudApotemas() {
 
@@ -43,7 +43,7 @@ public class Triangulo extends FiguraRegular implements IfigulaGeometricaR{
     JOptionPane.showMessageDialog(null, "la longitud del apotema de este triangulo es :"+longitudP);
     }
 
-    // los angulos de un tringulo son 180° independientemente de su clasificacion, en este caso equilatero
+    // metodo sobre escrito, los angulos de un tringulo son 180° independientemente de su clasificacion, en este caso un equilatero
     @Override
     public void sumaAngulosInteriores() {
 
